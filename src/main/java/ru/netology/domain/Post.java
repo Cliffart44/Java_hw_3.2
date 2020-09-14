09f064f2614f6ac75a9d9a9a3e33ab8dfa0e38cd
Cliffart <Cliffart44@gmail.com>
2020-09-14 13:15:52 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class Post {
+    private String postLogoURL;
+    private String profileName;
+    private String reportButton;
+    private String date;
+    private String mainText;
+    private String imageURL;
+    private String likesCount;
+    private String commentButton;
+    private String repostButton;
+    private String viewsCount;
+}
